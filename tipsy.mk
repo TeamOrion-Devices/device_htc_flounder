4a37391e8a29e3f69be5e36b4468903043c7c268
martin coulon <martinusbe@gmail.com>
2015-07-12 21:04:00 +0200

    Update and rename cm.mk to tipsy.mk

--- tipsy.mk
+++ tipsy.mk
@@ -1,14 +1,14 @@
-# CyanogenMod Specific Changes
+# Tipsy Specific Changes
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2048
 TARGET_SCREEN_WIDTH := 1536
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common Tipsy stuff.
+$(call inherit-product, vendor/tipsy/config/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/tipsy/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
@@ -16,7 +16,7 @@ $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
 $(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
 
 # Inline kernel building
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.8/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/htc/flounder
 TARGET_KERNEL_CONFIG := flounder_defconfig
@@ -40,6 +40,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="flounder-user 5.1.1 LMY47X 1849464 release-keys"
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_flounder
+PRODUCT_NAME := tipsy_flounder
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 9
+PRODUCT_MANUFACTURER := htc
+PRODUCT_RESTRICT_VENDOR_FILES := false
