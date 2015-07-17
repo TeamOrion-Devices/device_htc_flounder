55305ce9cbf7bee75481602c3ce88dac2011ae86
martinusbe <martinusbe@gmail.com>
2015-07-18 00:54:04 +0200

    change defconfig for aicp kernel
    Change-Id: Ie7d03ad58bbe49bb3394ef0bb07bf49ef6091b3d
    Signed-off-by: martinusbe <martinusbe@gmail.com>

--- orion.mk
+++ orion.mk
@@ -19,7 +19,7 @@ $(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.8/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/htc/flounder
-TARGET_KERNEL_CONFIG := lupus_defconfig
+TARGET_KERNEL_CONFIG := flounder_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
 
